18b5e4cb487291ad3f92fce663edf68f0bd353cc
Jaikeerthi <jai@coducer.com>
2023-06-07 01:17:20 +0530

    init chakra ui pro

---
+++ app/provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { theme as proTheme } from "@chakra-ui/pro-theme";
+import {
+  ChakraProvider,
+  extendTheme,
+  theme as baseTheme,
+} from "@chakra-ui/react";
+
+export const theme = extendTheme(
+  {
+    colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
+  },
+  proTheme
+);
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
