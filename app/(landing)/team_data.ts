d30b3d743c9a67e9348c371491b281e047e0e24d
Jaikeerthi <jai@coducer.com>
2023-06-12 03:39:19 +0530

    fix data issue and add title

--- app/(landing)/team_data.ts
+++ app/(landing)/team_data.ts
@@ -4,6 +4,7 @@ export const members = [
     image:
       "https://media.licdn.com/dms/image/C4D03AQHNMZ0IxUAK4Q/profile-displayphoto-shrink_800_800/0/1516784950470?e=1692230400&v=beta&t=9qgDo07d4J-ZYaMT3tIVfnyUHwm8DBsQ_2eTFqeBiWo",
     name: "Jaikeerthi K",
+    description: "",
     // description: 'Product evangelist, developer, startups',
   },
   {
@@ -11,6 +12,7 @@ export const members = [
     image:
       "https://media.licdn.com/dms/image/D4E03AQFHV_ilGnhUew/profile-displayphoto-shrink_800_800/0/1663508078590?e=1692230400&v=beta&t=3hckZtjvz_7ZxngKUHvg0y_v52yALKqU4bm_7nf5MQI",
     name: "Manoj Bharadwaj",
+    description: "",
     // description: 'Technology consulting, startups, advisory role',
   },
   {
@@ -18,12 +20,14 @@ export const members = [
     image:
       "https://media.licdn.com/dms/image/D5603AQEkT4aTJ_YYoQ/profile-displayphoto-shrink_800_800/0/1682336104038?e=1692230400&v=beta&t=k6Jy3PdUg14vcpNZkcw-xvcwuO-2WaKPxp4MYwctlvc",
     name: "Tejesh Pichandi ",
+    description: "",
   },
   {
     role: "Developer",
     image:
       "https://media.licdn.com/dms/image/C4D03AQHNMZ0IxUAK4Q/profile-displayphoto-shrink_800_800/0/1516784950470?e=1692230400&v=beta&t=9qgDo07d4J-ZYaMT3tIVfnyUHwm8DBsQ_2eTFqeBiWo",
     name: "Shabhari Saravanan",
+    description: "",
     // description: 'Quis risus sed vulputate odio ut enim blandit volutpat. Amet cursus sit amet.',
   },
   {
@@ -31,6 +35,6 @@ export const members = [
     image:
       "https://media.licdn.com/dms/image/C4E03AQGxAm4vp5XNfA/profile-displayphoto-shrink_800_800/0/1533258670816?e=1692230400&v=beta&t=AG0TI46nYvtk-OoGWheF72RfiIlzvIgfy6Kuu678shY",
     name: "Nikhil Lokhande",
-    // description: 'Consectetur libero id faucibus nisl tincidunt eget nullam fringilla urna.',
+    description: "",
   },
 ];
